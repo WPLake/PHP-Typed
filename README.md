abac6fa19122dc78d0d0cfcfbd40377e76e805b7
Maxim Akimov <lightsourcee@pm.me>
2024-12-20 15:23:28 +0200

    v 1.0.0

---
+++ README.md
@@ -0,0 +1,189 @@
+# PHP Typed
+
+> `Typed` is a lightweight PHP utility for seamless type-casting and data retrieval from dynamic variables, arrays, and
+> objects.
+
+This package provides a single `Typed` class with static methods and offers compatibility with PHP versions `7.4+` and
+`8.0+`.
+
+## 1. Why Use Typed?
+
+Handling type casting in PHP often leads to verbose and repetitive constructions. `Typed` streamlines this process,
+allowing you to fetch and cast values with concise, readable code.
+
+**Example: Plain PHP**
+
+```php
+function getTypedStringFromMixedVariable($mixed): string
+{
+    return true === is_string($mixed) || true === is_numeric($mixed)
+        ? (string)$mixed
+        : '';
+}
+
+function getTypedIntFromArray(array $array): int
+{
+    return true === isset($array['meta']['number']) &&
+             true === is_numeric($array['meta']['number'])
+             ? (int)$array['meta']['number']
+             : 0;
+}
+```
+
+**The same with `Typed` utility**
+
+```php
+use WPLake\Typed\Typed;
+
+function getTypedStringFromMixedVariable($mixedData): string
+{
+    return Typed::string($mixedData);
+}
+
+// Array item type casting:
+function getTypedIntFromArray(array $data): int
+{
+    return Typed::int($data, 'meta.number');
+}
+
+```
+
+Want to provide a default value when the key is missing? Here you go:
+
+```php
+Typed::string($data, 'some.key', 'Default Value');
+```
+
+## 2. Installation
+
+Typed class is distributed as a Composer package, making installation straightforward:
+
+`composer require wplake/typed`
+
+After installation, ensure that your application includes the Composer autoloader (if it hasn’t been included already):
+
+`require __DIR__ . '/vendor/autoload.php';`
+
+## 3. Supported types
+
+Static methods for the following types are present:
+
+* `string`
+* `int`
+* `float`
+* `bool`
+* `array`
+* `object`
+* `DateTime`
+
+For optional cases, each type has an `OrNull` method option (e.g. `stringOrNull`), which returns `null` if the key
+doesn’t exist.
+
+## 4. How It Works
+
+The logic of all casting methods follows this simple principle:
+
+> “Provide me a value of the requested type from the given source by the given path, or return the default value.”
+
+For example, let's review the `string` method declaration:
+
+```php
+namespace WPLake\Typed;
+
+class Typed {
+public static function string($source, $key = null, string $default = ''): string;
+// ...
+}
+```
+
+Usage Scenarios:
+
+1. Extract a string from a mixed variable (returning the default if absent or of an incompatible type)
+
+```php
+$userName = Typed::string($unknownVar);
+```
+
+2. Retrieve a string from an array, including nested structures using dot notation (e.g., `some.key.subkey`).
+
+```php
+$userName = Typed::string($array, 'user.name');
+```
+
+3. Access a string from an object, supporting nested properties with dot notation (e.g., `some.key.subkey`).
+
+```php
+$userName = Typed::string($companyObject, 'user.name');
+```
+
+4. Work with mixed structures (e.g., `object->arrayProperty['key']->anotherProperty or ['key' => $object]`).
+
+```php
+$userName = Typed::string($companyObject,'users.john.name');
+```
+
+## 5. FAQ
+
+### 5.1) Why not just Null Coalescing Operator?
+
+While the Null Coalescing Operator (`??`) is useful, it doesn’t address type checking or casting requirements.
+
+```php
+// Plain PHP:
+$number = $data['meta']['number']?? 10;
+$number = true === is_numeric($number)?
+(int)$number:
+10;
+
+// Typed:
+$number = Typed::int($data, 'meta.number', 10);
+```
+
+Additionally, with Null Coalescing Operator and a custom default value, you have to repeat yourself.
+
+### 5.2) Shouldn't we use typed objects instead?
+
+OOP is indeed powerful, and you should always prioritize using objects whenever possible. However, the reality is that
+our code often interacts with external dependencies beyond our control.
+
+This package simplifies handling such scenarios.
+Any seasoned PHP developer knows the pain of type-casting when working with environments outside of frameworks like
+WordPress.
+
+### 5.3) Is the dot syntax in keys inspired by Laravel Collections?
+
+Yes, the dot syntax is inspired by Laravel Collections and similar solutions. It provides an intuitive way to access
+nested data structures.
+
+### 5.4) Why not just use Laravel Collections?
+
+Laravel Collections and similar libraries don’t offer type-specific methods like this package does.
+
+While extending
+Laravel Collections could be a theoretical solution, we opted for a standalone package because:
+
+1. PHP Version Requirements: Laravel Collections require PHP 8.2+, while Typed supports PHP 7.4+.
+2. Dependencies: Laravel Collections bring several external Laravel-specific dependencies.
+3. Global Functions: Laravel Collections rely on global helper functions, which are difficult to scope when needed.
+
+## 6. Contribution
+
+We would be excited if you decide to contribute 🤝
+
+Please open Pull Requests against the `main` branch.
+
+### Code Style Agreements:
+
+#### 6.1) PSR-12 Compliance
+
+Use the [PHP_CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) tool in your IDE with the provided `phpcs.xml`,
+or run `composer phpcbf` to format your code.
+
+#### 6.2) Static Analysis with PHPStan
+
+Set up [PHPStan](https://phpstan.org/) in your IDE with the provided `phpstan.neon`, or run `composer phpstan` to
+validate your code.
+
+#### 6.3) Unit Tests
+
+[Pest](https://pestphp.com/) is setup for Unit tests. Run them using `composer pest`.
---
+++ tests/Unit/TypedTest.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use WPLake\Typed\Typed;
+use stdClass;
+
+class TypedTest extends TestCase
+{
+    // Note: All the class methods use the 'any()' method, so we can focus solely on it.
+
+    // border cases
+
+    public function testAnyReturnsSourceWhenKeyIsNotSet(): void
+    {
+        $result = Typed::any('origin');
+
+        $this->assertSame('origin', $result);
+    }
+
+    public function testAnyReturnsNullWhenSourceIsNotIterable(): void
+    {
+        $result = Typed::any('origin', 'key');
+
+        $this->assertNull($result);
+    }
+
+    public function testAnyReturnsDefaultWhenSourceIsNotIterable(): void
+    {
+        $result = Typed::any('origin', 'key', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    public function testAnyWorksWithMixedKeys(): void
+    {
+        $result = Typed::any([
+            0 => [
+                'key' => [
+                    '0' => 'value'
+                ]
+            ],
+        ], '0.key.0');
+
+        $this->assertSame('value', $result);
+    }
+
+    // arrays
+
+    public function testAnyMethodWorksWithArray(): void
+    {
+        $result = Typed::any(['key' => 'value'], 'key', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingKeyInArray(): void
+    {
+        $data = ['key' => 'value'];
+
+        $result = Typed::any($data, 'missing', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    public function testAnyMethodWorksWithArrayAndInnerKeys(): void
+    {
+        $data = ['level1' => ['level2' => ['key' => 'value']]];
+
+        $result = Typed::any($data, 'level1.level2.key', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingInnerKeyInArray(): void
+    {
+        $data = ['level1' => ['level2' => []]];
+
+        $result = Typed::any($data, 'level1.level2.missing', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    // objects with typed properties
+
+    public function testAnyMethodWorksWithObjectWithTypedProperties(): void
+    {
+        $object = new class {
+            public string $key = 'value';
+        };
+
+        $result = Typed::any($object, 'key', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingKeyInObjectWithTypedProperties(): void
+    {
+        $object = new class {
+            public ?string $key = null;
+        };
+
+        $result = Typed::any($object, 'key', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    public function testAnyMethodWorksWithObjectWithTypedInnerProperties(): void
+    {
+        $object = new class {
+            public object $level1;
+
+            public function __construct()
+            {
+                $this->level1 = new class {
+                    public object $level2;
+
+                    public function __construct()
+                    {
+                        $this->level2 = new class {
+                            public string $key = 'value';
+                        };
+                    }
+                };
+            }
+        };
+
+        $result = Typed::any($object, 'level1.level2.key', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingTypedInnerPropertiesInObject(): void
+    {
+        $object = new class {
+            public object $level1;
+
+            public function __construct()
+            {
+                $this->level1 = new class {
+                    public object $level2;
+
+                    public function __construct()
+                    {
+                        $this->level2 = new class {
+                            public ?string $key = null;
+                        };
+                    }
+                };
+            }
+        };
+
+        $result = Typed::any($object, 'level1.level2.key', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    // objects with dynamic properties
+
+    public function testAnyMethodWorksWithObjectWithDynamicProperties(): void
+    {
+        $object = new stdClass();
+        $object->key = 'value';
+
+        $result = Typed::any($object, 'key', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingKeyInObjectWithDynamicProperties(): void
+    {
+        $object = new stdClass();
+
+        $result = Typed::any($object, 'missing', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    public function testAnyMethodWorksWithObjectWithDynamicInnerProperties(): void
+    {
+        $object = new stdClass();
+        $object->level1 = new stdClass();
+        $object->level1->level2 = new stdClass();
+        $object->level1->level2->key = 'value';
+
+        $result = Typed::any($object, 'level1.level2.key', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingDynamicInnerPropertiesInObject(): void
+    {
+        $object = new stdClass();
+        $object->level1 = new stdClass();
+        $object->level1->level2 = new stdClass();
+
+        $result = Typed::any($object, 'level1.level2.missing', 'default');
+
+        $this->assertSame('default', $result);
+    }
+
+    // mixed cases
+
+    public function testAnyMethodWorksWithMixedStructures(): void
+    {
+        $object = new stdClass();
+        $object->property = new stdClass();
+        $object->property->values = [
+            'arrayKey' => [
+                'innerObject' => new stdClass()
+            ]
+        ];
+        $object->property->values['arrayKey']['innerObject']->property = 'value';
+
+        $result = Typed::any($object, 'property.values.arrayKey.innerObject.property', 'default');
+
+        $this->assertSame('value', $result);
+    }
+
+    public function testAnyMethodReturnsDefaultForMissingKeyInMixedStructures(): void
+    {
+        $object = new stdClass();
+        $object->property = new stdClass();
+        $object->property->values = [
+            'arrayKey' => [
+                'innerObject' => new stdClass()
+            ]
+        ];
+
+        $result = Typed::any($object, 'property.values.arrayKey.innerObject.missing', 'default');
+
+        $this->assertSame('default', $result);
+    }
+}
